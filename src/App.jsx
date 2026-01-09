a1ce466a691713e08113bd4250820790f8cdda6f
Whisperer <whisperer.meta@gmail.com>
2026-01-09 16:50:19 +0200

    add hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import HeroSection from './components/HeroSection'
 
 export default function App() {
   return (
     <>
       <Header />
-      <main></main>
+      <main>
+        <HeroSection />
+      </main>
       <Footer />
     </>
   )
